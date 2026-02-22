a03a836d37e83a1c8365ed279fc9f2f1c907230a
Janaye Cheong <janaye.c13@gmail.com>
2026-02-22 14:52:06 -0800

    feat: added react brower

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </StrictMode>
+  </BrowserRouter>
 );
